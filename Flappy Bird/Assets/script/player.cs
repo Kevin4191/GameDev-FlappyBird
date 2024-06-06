6ded67c2b5a1abb3efcf787dd7f668b199f36800
lucaFrost99 <137787353+lucaFrost99@users.noreply.github.com>
2024-06-06 09:00:17 +0200

    project toevoegt

---
+++ Flappy Bird/Assets/script/player.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Vector3 direction;
+
+    public float gravity = -9.8f;
+    public float strength = 5f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            direction = Vector3.up * strength;
+        }
+
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
+    }
+}
